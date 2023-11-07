fb7359ad3ca97c3b4b6dae3e10817196695755e1
Alex <alexandre.ducr3@gmail.com>
2023-11-07 21:27:52 +0100

    tests qui se lancent

--- testCorpsFini.py
+++ testCorpsFini.py
@@ -1,4 +1,4 @@
-import CorpsFini
+from CorpsFini import CorpsFini
 import unittest
 
 class testCorpsFini(unittest.TestCase):
@@ -8,36 +8,36 @@ class testCorpsFini(unittest.TestCase):
         self.CF2=CorpsFini(5,7)
 
     def testRepr(self):
-        assertEqual(repr(self.corpsFini),'(3,7)')
+        self.assertEqual(repr(self.corpsFini),'(3,7)')
 
     def testEqual(self):
         assertFalse(self.CF1==self.CF2)
         assertTrue(self.CF1==self.CF1)
 
     def testAdd(self):
-        assertEqual(self.CF1+self.CF2,CorpsFini(1,7))
-        assertEqual(self.CF1+CorpsFini(2,7), self.CF2)
+        self.assertEqual(self.CF1+self.CF2,CorpsFini(1,7))
+        self.assertEqual(self.CF1+CorpsFini(2,7), self.CF2)
 
     def testSub(self):
-        assertEqual(self.CF2-self.CF1,CorpsFini(2,7))
-        assertEqual(self.CF1-self.CF1, CorpsFini(0,7))
+        self.assertEqual(self.CF2-self.CF1,CorpsFini(2,7))
+        self.assertEqual(self.CF1-self.CF1, CorpsFini(0,7))
     
     def testMul(self):
-        assertEqual(self.CF1*self.CF2,CorpsFini(1,7))
-        assertEqual(self.CF1*CorpsFini(2,7), CorpsFini(6,7))
+        self.assertEqual(self.CF1*self.CF2,CorpsFini(1,7))
+        self.assertEqual(self.CF1*CorpsFini(2,7), CorpsFini(6,7))
 
     #TODO: test de la division 
     def testTrueDiv(self):
         pass
 
     def testPow(self):
-        assertEqual(self.CF1**0,CorpsFini(1,7))
-        assertEqual(self.CF1**1,self.CF1)
-        assertEqual(self.CF1*self.CF1,self.CF1**2)
-        assertEqual(self.CF2**3, CorpsFini((5**3)%7,7))
+        self.assertEqual(self.CF1**0,CorpsFini(1,7))
+        self.assertEqual(self.CF1**1,self.CF1)
+        self.assertEqual(self.CF1*self.CF1,self.CF1**2)
+        self.assertEqual(self.CF2**3, CorpsFini((5**3)%7,7))
 
 
-
-
+if __name__ == '__main__':
+        unittest.main(verbosity=2)
 
 
