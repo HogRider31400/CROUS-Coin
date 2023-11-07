04e60001089ccf90b764ecaccf401d6e9619e98d
D33333 <dc3comics@gmail.com>
2023-11-07 22:20:16 +0100

    Tests Fonctionnels, grand pas pour nous

--- CorpsFini.py
+++ CorpsFini.py
@@ -5,13 +5,13 @@ class CorpsFini:
 
     def __init__(self, nb, nb_premier):
         if nb >= nb_premier or nb<0:
-            error = str(nb)+" n'est pas dans le corps (entre 0 et "+str(nb_premier -1)
+            error = str(nb)+" n'est pas dans le corps (entre 0 et "+str(nb_premier -1)+")"
             raise ValueError(error)
         self.nb = nb
         self.nb_premier = nb_premier
 
     def __repr__(self):
-        return str(self.nb_premier, self.nb)
+        return str((self.nb,self.nb_premier))
 
     def __eq__(self, other):
         if other is None:
@@ -23,17 +23,17 @@ class CorpsFini:
             raise TypeError('On ne peut pas additionner 2 nombres de corps différents')
 
     def __add__(self, other):
-        primeValid(self, other)
+        self.primeValid(other)
         nb = (self.nb + other.nb) % self.nb_premier
         return self.__class__(nb, self.nb_premier)
 
     def __sub__(self, other):
-        primeValid(self, other)
+        self.primeValid(other)
         nb = (self.nb - other.nb) % self.nb_premier
         return self.__class__(nb, self.nb_premier)
 
     def __mul__(self, other):
-        primeValid(self, other)
+        self.primeValid(other)
         nb = (self.nb * other.nb) % self.nb_premier
         return self.__class__(nb, self.nb_premier)
 
@@ -44,11 +44,11 @@ class CorpsFini:
     def __truediv__(self, other):
         '''Fait la division flottante entre a et b : a/b, de toute manière
         le résultat sera entier'''
-        primeValid(self, other)
+        self.primeValid(other)
         if (other.nb == 0):
             raise ZeroDivisionError("le diviseur doit être non nul")
         ##Calcul de l'inverse : b^(-1) = b^(nb_premier-2)
-        inverse_op2 = other.nb ** ((other.nb_premier)-2)
+        inverse_op2 = (other.nb ** ((other.nb_premier)-2))%self.nb_premier
         return self * self.__class__(inverse_op2, self.nb_premier)
 
     def __pow__(self, exposant):
--- testCorpsFini.py
+++ testCorpsFini.py
@@ -1,8 +1,6 @@
 from CorpsFini import CorpsFini
 import unittest
 
-import CorpsFini
-
 class testCorpsFini(unittest.TestCase):
 
     def setUp(self):
@@ -12,11 +10,11 @@ class testCorpsFini(unittest.TestCase):
         self.CF3=CorpsFini(1,5)
 
     def testRepr(self):
-        self.assertEqual(repr(self.corpsFini),'(3,7)')
+        self.assertEqual(repr(self.CF1),"(3, 7)")
 
     def testEqual(self):
-        assertFalse(self.CF1==self.CF2)
-        assertTrue(self.CF1==self.CF1)
+        self.assertFalse(self.CF1==self.CF2)
+        self.assertTrue(self.CF1==self.CF1)
 
     def testAdd(self):
         self.assertEqual(self.CF1+self.CF2,CorpsFini(1,7))
@@ -25,17 +23,17 @@ class testCorpsFini(unittest.TestCase):
     def testSub(self):
         self.assertEqual(self.CF2-self.CF1,CorpsFini(2,7))
         self.assertEqual(self.CF1-self.CF1, CorpsFini(0,7))
-    
+
     def testMul(self):
         self.assertEqual(self.CF1*self.CF2,CorpsFini(1,7))
         self.assertEqual(self.CF1*CorpsFini(2,7), CorpsFini(6,7))
 
     def testTrueDiv(self):
-        assertEqual(self.CF1/self.CF2, CorpsFini(3*5**(5),7))
+        self.assertEqual(self.CF1/self.CF2, CorpsFini(((3*5**(5))%7),7))
         with self.assertRaises(TypeError):
             self.CF1/self.CF3
         with self.assertRaises(ZeroDivisionError):
-            self.CF1/self.CFO
+            self.CF1/self.CF0
 
     def testPow(self):
         self.assertEqual(self.CF1**0,CorpsFini(1,7))
