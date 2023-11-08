8d2246fe72baa6ccea287c11b955472a700f96f5
Alex <alexandre.ducr3@gmail.com>
2023-11-08 20:48:56 +0100

    Début débug, sauvegarde sur pc, ajout fonction vérification et beaucoup de choses

--- ClePrivee.py
+++ ClePrivee.py
@@ -1,8 +1,21 @@
 from random import randint
+from Point import Point
+def ordre_point(point):
+    cur_point = point
+    nb = 1
+    while not (cur_point == Point(None,None,point.a,point.b,point.nb_premier)):
+        print(cur_point.x,cur_point.y)
+        cur_point += point
+        nb += 1
+    return nb
+
+print(ordre_point(Point(15,86,0,7,223)))
 
 class ClePrivee:
     ###### à mettre chez le corps fini comme variable globale
     PREMIER = 512
+    G = (1,1) #A définir
+    N = 1 #A definir, ordre du point dans la courbe
 
     #Attributs : e, le secret
 
@@ -12,16 +25,23 @@ class ClePrivee:
 
     def hex(self):
         return e.zfill(64)
+    
+    def verifier(self,z,signature):
+        s_inverse = pow(signature.s,N-2,N)
+        u = z*s_inverse%N
+        v = signature.r*s_inv%N
+        total = u*G + v*self
+        return total.x == signature.r
 
     def signer(self, z):
         ####---------------------------------------------
         ## Déterminer un meilleur moyen d'avoir k
         ###----------------------------------------------
         ######RENDRE K UNIQUE, cf la suite
-        k = randint(0, 2**PREMIER)
+        k = randint(0, 2**N)
         r = (k*G).x.nb
-        k_inverse = pow(k, PREMIER-2, PREMIER)
-        s = ((z+r*self.e)*k_inverse) % PREMIER
-        if s > PREMIER/2:
-            s = PREMIER - s
+        k_inverse = pow(k, N-2, N)
+        s = ((z+r*self.e)*k_inverse) % N
+        if s > N/2:
+            s = N - s
         return Signature(r, s)
--- Point.py
+++ Point.py
@@ -1,32 +1,47 @@
+import CorpsFini
 
 class Point:
     
     #potentiellement remplacer a et b par la courbe elliptique
-    def __init__(self,x,y,a,b):
-        self.a = a
-        self.b = b
-        self.x = x
-        self.y = y
+    def __init__(self,x,y,a,b,nb_premier):
+        if not type(a) is CorpsFini.CorpsFini:
+            self.a = CorpsFini.CorpsFini(a,nb_premier)
+        else:
+            self.a = a
+        if not type(b) is CorpsFini.CorpsFini:
+            self.b = CorpsFini.CorpsFini(b,nb_premier)
+        else:
+            self.b = b
+        self.nb_premier = nb_premier
+        if not type(x) is CorpsFini.CorpsFini:
+            self.x = CorpsFini.CorpsFini(x,nb_premier)
+        else:
+            self.x = x
+        if not type(y) is CorpsFini.CorpsFini:
+            self.y = CorpsFini.CorpsFini(y,nb_premier)
+        else:
+            self.y = y
 
-        if self.x is None and self.y is None:
+        if x is None and y is None:
             return
-        self.id = Point(None,None,a,b)
+        self.id = Point(None,None,a,b,self.nb_premier)
 
         #vérifier que (x,y) est sur la courbe
-        if y*3!=x**2+a*x+b:
+        if self.y**2!=self.x**3+self.a*self.x+self.b:
             raise ValueError("le point n'est pas sur la courbe.")
     
     def __eq__(self,other):
         return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b
+
 
     def __add__(self,other_point):
         #0eme cas : les deux ne sont pas sur la même courbe, à faire quand mael aura fait la classe
 
 
         #1er cas l'un des deux est l'identité
-        if self.x is None:
+        if self.x.nb is None:
             return other_point
-        if other_point.x is None:
+        if other_point.x.nb is None:
             return self
 
         #2eme cas ils sont inverses
@@ -36,9 +51,12 @@ class Point:
         #3eme cas les points ne sont pas égaux
         if self.x != other_point.x:
             slope = (other_point.y - self.y)/(other_point.x-self.x)
+            print(slope)
             new_x = slope**2 - self.x - other_point.x
+            print(new_x)
             new_y = slope*(self.x-new_x) - other_point.y
-            return Point(new_x,new_y,self.a,self.b)
+            print("aled",new_x,new_y,self.x,self.y,other_point.x,other_point.y)
+            return Point(new_x,new_y,self.a.nb,self.b.nb,self.nb_premier)
         
         #4eme cas les points sont égaux et la slope est en +inf
         if self==other_point and self.y == 0*self.x:
@@ -50,10 +68,10 @@ class Point:
             slope = (3*self.x**2 + self.a)/(2*self.y)
             new_x = slope**2 - 2*self.x
             new_y = slope*(self.x-new_x) - self.y
-            return Point(new_x,new_y,self.a,self.b)
+            return Point(new_x,new_y,self.a.nb,self.b.nb,self.nb_premier)
     
-    def __mul__(self,scalaire):
-        resultat = Point(None,None,self.a,self.b)
+    def __rmul__(self,scalaire):
+        resultat = Point(None,None,self.a.nb,self.b.nb,self.nb_premier)
         for i in range(scalaire):
             resultat += self
         return resultat
