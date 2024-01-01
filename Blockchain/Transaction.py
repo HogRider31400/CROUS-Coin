f3f4d38a64c405ace737969155f58559581d9b8c
D33333 <dc3comics@gmail.com>
2024-01-01 18:09:56 +0100

    Changement de forme Transaction

--- Blockchain/Transaction.py
+++ Blockchain/Transaction.py
@@ -1,45 +1,67 @@
 import json
 import time
 import sys
+import utils
 sys.path.append("..")
+from utils.py import *
 from Signature import Signature
+from Point import Point
+
+N = order
+G = Point(generator_x,generator_y)
 
 class Transaction:
 
-    #Attributs : horodatage, inputs, outputs
-    outputs = []
+    #Attributs : horodatage, inputs, outputs, nbInputs, nbOutputs, adresse
 
-    ##Une input est de la forme : {acheteur:...(adresse),montant:...,sigVendeur:...(signature),cleVendeur:...(Px,Py)}
-    ##Une output est de la forme : {vendeur:...,montant:...,sigAcheteur:...,cleAcheteur:...(Px,Py)}
+    ##Une input est de la forme : {montant:...,sigAcheteur:...(signature),cleAcheteur:...(Px,Py)}
+    ##Une output est de la forme : {vendeur:...,montant:...,sigVendeur:...,cleVendeur:...(Px,Py)}
+
+    '''
+    Principe :
+        Lorsque Bob veut acheter quelque chose avec des CrousCoin, il amène sur
+        la table l'ensemble des transactions dont il ne s'est pas servi où il a reçu
+        de l'argent. Ce sont les inputs.
+
+        Dans une input il y a l'adresse de celui qui lui a acheté qqch (ou rendu un
+        service dans la vraie vie), le montant de l'achat, l'adresse de Bob (qui avait le
+        rôle de vendeur), et la signature de Bob.
+
+        On rajoute pour plus de commodité la clé publique de Bob dans chaque input.
+
+        ->B->C
+        et dans l'UTXO set : ->C
+    '''
 
     #S'ASSURER QUE LES INPUTS VIENNENT BIEN TOUTES DE L'UTXO SET
 
-    def __init__(self,inputs, outputs):
+    def __init__(self,inputs, outputs, adresseAcheteur):
         self.horodatage = time.time()
         self.inputs = inputs
         self.outputs = outputs
         self.nbInputs = len(inputs)
         self.nbOutputs = len(outputs)
+        self.adresseAcheteur = adresseAcheteur
 
     @classmethod
     def from_text(cls,text):
         bloc_data = json.loads(text)
 
-        horodatage = bloc_data["horodatage"]
-        inputs = bloc_data["inputs"]
-        outputs = bloc_data["outputs"]
+        self.horodatage = bloc_data["horodatage"]
+        self.inputs = bloc_data["inputs"]
+        self.outputs = bloc_data["outputs"]
 
-        return cls(horodatage,inputs,outputs)
+        return cls(self.horodatage,self.inputs,self.outputs)
 
     def __repr__(self):
-        return "Transaction ("+horodatage+"-I:"+afficherIO(inputs)+"-O:"+afficherIO(outputs)+')'
+        return "Transaction ("+self.horodatage+"-I:"+afficherIO(self.inputs)+"-O:"+afficherIO(self.outputs)+')'
 
     def ajouterInputs(newInputs):
         '''Entrée : prend un tableau d'inputs, (les vérifie une à une?) puis les ajoute
         dans les inputs de la transaction'''
         ##vérifier que dans l'UTXO set
-        inputs = inputs + newInputs
-        nbInputs += len(newInputs)
+        self.inputs = self.inputs + newInputs
+        self.nbInputs += len(newInputs)
 
     def enleverInputs(delInputs):
         trouve = False
@@ -47,8 +69,8 @@ class Transaction:
         for oldInput in delInputs:
             i=0
             while (i<nbInputs and not trouve):
-                if (inputs[i] == oldInput):
-                    del inputs[i]
+                if (self.inputs[i] == oldInput):
+                    del self.inputs[i]
                     trouve = True
                     result.append(True)
                 i+=1
@@ -61,17 +83,17 @@ class Transaction:
         '''Entrée : prend un tableau d'outputs, (les vérifie une à une?) puis les ajoute
         dans les outputs de la transaction'''
         ##vérifier que dans l'UTXO set
-        outputs = outputs + newOutputs
-        nbOutputs += len(newOutputs)
+        self.outputs = self.outputs + newOutputs
+        self.nbOutputs += len(newOutputs)
 
     def enleverOutputs(delOutputs):
         trouve = False
         result = []
         for output in delOutputs:
             i=0
-            while (i<nbOutputs and not trouve):
-                if (outputs[i] == output):
-                    del outputs[i]
+            while (i<self.nbOutputs and not trouve):
+                if (self.outputs[i] == output):
+                    del self.outputs[i]
                     trouve = True
                     result.append(True)
                 i+=1
@@ -86,16 +108,16 @@ class Transaction:
         bills = ""
         for bill in tabIO:
             bills += str(bill)+','
-        if (bills.length()!=0):
+        if (len(bills)!=0):
             bills = bills[:-1]
         return bills
 
     def differenceIO():
         sommeI = 0
         sommeO = 0
-        for billI in inputs:
+        for billI in self.inputs:
             sommeI += billI["montant"]
-        for billO in outputs:
+        for billO in self.outputs:
             sommeO += billO["montant"]
         return sommeI - sommeO
 
@@ -108,39 +130,50 @@ class Transaction:
     def sommePositive():
         return differenceIO()>=0
 
+    def hasherMsg(msg):
+        zh = s256('sample'.encode('utf-8')).digest()
+        z = utils.get_int(zh,N)
+        return z
+
     def verifierSignatures():
         return verifierSigInputs() and verifierSigOutputs()
 
     def verifierSigInputs():
         valide = True
-        for bill in inputs:
-            valide = valide and bill["sigVendeur"].verifier()
+        for bill in self.inputs:
+            P = bill["cleVendeur"]
+            creerMsg(self.horodatage,bill["acheteur"],bill["montant"],self.adresseAcheteur)
+            valide = valide and bill["sigVendeur"].verifier(hasherMsg(msg),G,N,P)
         return valide
 
     def verifierSigOutputs():
         valide = True
-        for bill in outputs:
-            valide = valide and bill["sigAcheteur"].verifier()
+        for bill in self.outputs:
+            P = bill["cleAcheteur"]
+            creerMsg(self.horodatage,bill["vendeur"],bill["montant"],bill["vendeur"])
+            valide = valide and bill["sigAcheteur"].verifier(hasherMsg(msg),G,N,P)
         return valide
 
+    def creerMsg(horodatage,montant,adresse):
+        return str(horodatage)+'#'+adresse+'#'+montant
 
 
     ##Getteurs
     #Pas de set sur l'horodatage pour éviter les fraudes
 
     def getHorodatage():
-        return horodatage
+        return self.horodatage
 
     def getInputs():
-        return inputs
+        return self.inputs
 
     def getOutputs():
-        return outputs
+        return self.outputs
 
     def setInputs(newInputs):
-        inputs = newInputs
-        nbInputs = len(newInputs)
+        self.inputs = newInputs
+        self.nbInputs = len(newInputs)
 
     def setOutputs(newOutputs):
-        outputs = newOutputs
-        nbOutputs = len(newOutputs)
+        self.outputs = newOutputs
+        self.nbOutputs = len(newOutputs)
